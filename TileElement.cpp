a3f77d69add6def86620df66bd91241a23e9d04b
Esteban Daude <estebdaude@gmail.com>
2024-02-02 11:28:40 +0100

    Implementation des objets en sous classes

--- TileElement.cpp
+++ TileElement.cpp
@@ -3,3 +3,33 @@
 char TileElement :: printElement(){
     return rep;
 }
+
+Potion :: Potion(string s, int n){
+    rep = 'j';
+    name = s;
+    heal = n;
+}
+
+Gold :: Gold(string s, int n){
+    rep = '*';
+    value = n;
+    name = s;
+}
+
+Weapon :: Weapon(string s, int n){
+    rep = '!';
+    damage = n;
+    name = s;
+}
+
+Armor :: Armor(string s, int n){
+    rep = 'a';
+    name = s;
+    protection = n;
+}
+
+Food :: Food(string s, int n){
+    rep = 'f';
+    name = s;
+    staminaPoints = n;
+}
--- TileElement.hpp
+++ TileElement.hpp
@@ -2,20 +2,54 @@
 #define __TILEELEMENT_H
 
 #include <iostream>
+#include <string>
+using namespace std;
+
 
 class TileElement{
 protected :
     char rep;
 public :
     char printElement ();
+    TileElement(){};
+    ~TileElement(){};
 };
 
 class Terrain: TileElement{
 };
 
-class Object: TileElement{
+class Object: public TileElement{
+protected :
+    string name;
+public :
+    Object(){};
+    ~Object(){};
 };
 
+class Potion : public Object{
+    int heal; 
+    Potion(string s, int n);
+};
+
+class Gold : public Object{
+    int value;
+    Gold(string s, int n);
+};
+
+class Weapon : public Object{
+    int damage;
+    Weapon(string s, int n);
+};
+
+class Armor : public Object{
+    int protection;
+    Armor(string s, int n);
+};
+
+class Food : public Object{
+    int staminaPoints;
+    Food(string s, int n);
+};
 
 
 #endif
